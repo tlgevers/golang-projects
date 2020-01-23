72412f38a232bc2a603e6e2cdaa699b9674145b6
Trevor Gevers <trevor.gevers@ditoweb.com>
2020-01-23 09:17:18 -0600

    basic go client to return http response

--- calculator/client/main.go
+++ calculator/client/main.go
@@ -6,16 +6,20 @@ import (
     "time"
     "google.golang.org/grpc"
     pb "github.com/tlgevers/golang-projects/calculator/client/calc"
+    "net/http"
+    "fmt"
 )
 
 const (
     address = "localhost:50051"
 )
 
-func main() {
+
+func sum() (sum int32, err error) {
     conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
     if err != nil {
         log.Fatalf("did not connect: %v", err)
+        return
     }
     defer conn.Close()
     c := pb.NewCalcClient(conn)
@@ -26,6 +30,23 @@ func main() {
     r, err := c.Sum(ctx, &pb.Request{ArgA: 1, ArgB: 5})
     if err != nil {
         log.Fatalf("could not sum: %v", err)
+        return
     }
     log.Printf("Sum is: %v", r.GetResult())
+    sum = r.GetResult()
+    return
+}
+
+func handler(w http.ResponseWriter, request *http.Request) {
+    sum,err := sum()
+    if err != nil {
+        log.Fatalf("could not get sum: %v", err)
+    }
+    fmt.Printf("the sum is %v", sum)
+    fmt.Fprintf(w, "The sum is: %v", sum)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8081", nil)
 }
